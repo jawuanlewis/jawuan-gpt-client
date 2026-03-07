93da2f5a8e80182b29b1ac90c62da5b9a8915dc9
Jawuan Lewis <jawuanlewis@users.noreply.github.com>
2026-03-07 11:05:24 -0700

    Add services, styles, types, & utils

---
+++ src/services/apiClient.ts
@@ -0,0 +1,35 @@
+import axios, { AxiosError } from 'axios';
+import { getClientId } from '@src/utils/clientId';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+  config.headers['X-Client-ID'] = getClientId();
+  return config;
+});
+
+// Error handling
+api.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError) => {
+    if (error.response) {
+      console.error('API Response Error:', {
+        status: error.response.status,
+        message:
+          typeof error.response.data === 'object' &&
+          error.response.data !== null
+            ? (error.response.data as { message?: string }).message
+            : 'Unknown error',
+      });
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
